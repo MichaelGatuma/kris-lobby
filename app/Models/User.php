fe7d878622be250193132b474344652686dd07bf
Michael Gatuma <mgates4410@gmail.com>
2020-10-24 16:13:51 +0300

    Edit User Model, keep original model backup

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,28 @@ use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * @property int $id
+ * @property string $Title
+ * @property string $name
+ * @property string $email
+ * @property string $email_verified_at
+ * @property string $password
+ * @property string $two_factor_secret
+ * @property string $two_factor_recovery_codes
+ * @property string $profPic
+ * @property boolean $isAdmin
+ * @property string $remember_token
+ * @property integer $current_team_id
+ * @property string $profile_photo_path
+ * @property string $created_at
+ * @property string $updated_at
+ * @property string $verified_at
+ * @property Post[] $posts
+ * @property Publication[] $publications
+ * @property Researcher[] $researchers
+ * @property Researchproject[] $researchprojects
+ */
 class User extends Authenticatable
 {
     use HasApiTokens;
@@ -58,4 +80,36 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function posts()
+    {
+        return $this->hasMany('App\Models\Post');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function publications()
+    {
+        return $this->hasMany('App\Models\Publication', 'UserID');
+    }
+
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function researchers()
+    {
+        return $this->hasMany('App\Models\Researcher', 'User_ID');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function researchprojects()
+    {
+        return $this->hasMany('App\Models\Researchproject', 'User_ID');
+    }
 }

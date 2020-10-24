73b190ac06d650e8667b1be9e80159fe7ade0b17
Michael Gatuma <mgates4410@gmail.com>
2020-10-24 15:03:58 +0300

    Fix users table migration
    added: missing users_table columns migrations for laravel 5 compatibility
    removed: users_table migration

---
+++ database/migrations/2020_10_24_115317_add_columns_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnsToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->timestamp('email_verified_at')->nullable()->after('email');
+            $table->foreignId('current_team_id')->nullable()->after('remember_token');
+            $table->text('profile_photo_path')->nullable()->after('current_team_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}

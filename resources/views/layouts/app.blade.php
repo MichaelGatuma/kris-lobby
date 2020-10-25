a9f3807293df3641143678a4426b71c2a10998aa
Michael Gatuma <mgates4410@gmail.com>
2020-10-25 16:51:10 +0300

    Revert "add wrapper layout"
    This reverts commit 1cbbfa3268bb95c9ef58ba56bf215a85576173c2.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,13 +26,13 @@
     <!-- Page Heading -->
     <header class="bg-white shadow">
         <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-            {{ $header ?? null }}
+            {{ $header }}
         </div>
     </header>
 
     <!-- Page Content -->
     <main>
-        {{ $slot ?? null }}
+        {{ $slot }}
     </main>
 </div>
 

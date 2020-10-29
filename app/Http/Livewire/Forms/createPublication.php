99d4e10fbeee9462068fdfb9f21d2cba0ef11e12
Michael Gatuma <mgates4410@gmail.com>
2020-10-29 19:36:45 +0300

    add: publication form

--- app/Http/Livewire/Forms/createPublication.php
+++ app/Http/Livewire/Forms/createPublication.php
@@ -3,11 +3,14 @@
 namespace App\Http\Livewire\Forms;
 
 use App\Models\Publication;
+use App\Models\Researcher;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 use Livewire\WithFileUploads;
 use Tanthammar\TallForms\FileUpload;
 use Tanthammar\TallForms\Input;
+use Tanthammar\TallForms\Select;
 use Tanthammar\TallForms\TallForm;
 use Tanthammar\TallForms\Textarea;
 use Tanthammar\TallForms\Traits\UploadsFiles;
@@ -20,7 +23,8 @@ class createPublication extends Component
 
     public function mount(?Publication $publication)
     {
-
+        $publication = Publication::first();
+//        $this->model->User_ID=Researcher::find($this->form_data['Researcher_ID'])->User_ID;
         //Gate::authorize()
         $this->fill([
             'formTitle' => trans('global.create') . ' ' . trans('crud.publication.title_singular'),
@@ -36,6 +40,7 @@ class createPublication extends Component
     public function onCreateModel($validated_data)
     {
         // Set the $model property in order to conditionally display fields when the model instance exists, on saveAndStayResponse()
+        $this->model->User_ID = Researcher::find($validated_data['Researcher_ID'])->User_ID;
         $this->model = Publication::create($validated_data);
     }
 
@@ -45,27 +50,42 @@ class createPublication extends Component
         $this->model->update($validated_data);
     }
 
-    public function render()
+    public function savePublicationPath($validated_file)
     {
-        $createResearcher = new createResearcher;
-        $this->form_data['UserID'] = $createResearcher->uid;
-        return $this->formView();
+        $path = filled($validated_file) ? $this->PublicationPath->store('PublicationLists') : null;
+        //do something with the model?
+        if (optional($this->model)->exists && filled($path)) {
+            $this->model->PublicationPath = $path;
+            $this->model->save();
+        }
+    }
+
+    public function updatedResearcherID($validated_value)
+    {
+        $this->model = new Publication();
+        if (Publication::where('Researcher_ID', '=', $validated_value)->first() != null) {
+            $this->model = Publication::where('Researcher_ID', '=', $validated_value)->first();
+        } else {
+            $this->model = new Publication();
+        }
+        $this->model->User_ID = Researcher::find($validated_value)->User_ID;
+        $this->model->Researcher_ID = $validated_value;
+        $this->mount_form($this->model);
     }
 
     public function fields()
     {
         return [
-            Input::make('User ID', 'UserID')
-                ->rules('required')
-                ->wire('wire:model'),
+            Select::make('Researcher', 'Researcher_ID')
+                ->options($this->getResearchers())
+                ->rules('required'),
             Input::make('Publication Title', 'PublicationTitle')
-                ->rules('required')
-                ->wire('wire:model'),
-            FileUpload::make('Upload Publication', 'PublicationPath')
-                ->multiple()
-                ->help('Max 1024kb, png, jpeg, gif or tiff') //important for usability to inform about type/size limitations
-                ->rules('nullable|mimes:png,jpg,jpeg,gif,tiff|max:1024') //only if you want to override livewire main config validation
-                ->accept("pdf/*"),
+                ->rules('required'),
+            optional($this->model)->exists //you probably do not want to attach files if the model does not exist
+                ? FileUpload::make('Upload Publication', 'PublicationPath')
+                ->help('Max 5 megabytes, *.pdf (Existing Publication files will be replaced)') //important for usability to inform about type/size limitations
+                ->rules('nullable|mimes:pdf|max:5120') //only if you want to override livewire main config validation
+                ->accept(".pdf") : null,
             Input::make('Date of Publication', 'DateOfPublication')
                 ->type('date')
                 ->step(7)
@@ -76,12 +96,16 @@ class createPublication extends Component
             Textarea::make('Collaborators', 'Collaborators')
                 ->rows(3)
                 ->placeholder('')
-                ->rules('string')
-                ->wire('wire:model'),
+                ->rules('string'),
             Input::make('Publication url', 'PublicationURL')
                 ->type('url')
                 ->prefix('https://')
                 ->rules('active_url'),
         ];
+    }
+
+    public function getResearchers()
+    {
+        return collect(DB::table('researchers')->leftJoin('users', 'researchers.User_ID', 'users.id')->get())->pluck('Researcher_ID', 'name')->all();
     }
 }
